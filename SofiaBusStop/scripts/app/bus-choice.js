00c9a23ddf4c3bc4ebb7fdd10c53adf54d6a4140
Spas Tyurkedzhiev <harda1@abv.bg>
2013-10-10 11:31:17 -0500

    Changes made by Spas Tyurkedzhiev (harda1@abv.bg).

--- SofiaBusStop/scripts/app/bus-choice.js
+++ SofiaBusStop/scripts/app/bus-choice.js
@@ -2,22 +2,38 @@ var app = app || {};
 
 (function(a) {
     
-    function saveChoice(){
-        
-    }
-    
-    
+    var busNumber=0;
     
     var viewModel = kendo.observable({
-       saveChoice:saveChoice
+        categories:[],
+        selectedCategory:null,
+        change:onCategoryChanged,
+        getSearchData:getSearchData
     });
     
     function init(e) {
+        kendo.bind(e.view.element, viewModel);
+        httpRequest.getJSON("http://sofiabusapi.apphb.com/api/bus/")
+        .then(function (categories) {
+            viewModel.set("categories", categories.Busses);            
+        });   
+    }
+    
+    function getSearchData(){
+        var busValue=busNumber;
+        var selectValue=$('#category-info').val();
+        //Pass these two variables to the other view
+        var t=5;
+    }
+    
+    function onCategoryChanged(e) {             
+        busNumber=e.sender._selectedValue;
         
-          var p=5;
-          kendo.bind(e.view.element, viewModel);
-       // getNearestStop();
-    }   
+        httpRequest.getJSON("http://sofiabusapi.apphb.com/api/bus/" + e.sender._selectedValue)
+        .then(function(category) {
+            viewModel.set("selectedCategory", category);
+        });
+    }
     
     a.choice = {
         init:init          
